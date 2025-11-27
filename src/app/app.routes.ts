112dbe3a25284000207c49ad117b9ef34e0055bc
Om Dixit <omdixit2826@gmail.com>
2025-11-27 18:21:56 +0530

    Update package dependencies, enhance routing, and implement login/signup components with styles and dashboard layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,12 @@
 import { Routes } from '@angular/router';
-import { App } from './app';
+import { Login } from './component/login/login';
+import { SignUp } from './component/sign-up/sign-up';
+import { Dashboard } from './component/dashboard/dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path: 'login', component: Login },
+    { path: 'signup', component: SignUp },
+    { path: 'dashboard', component: Dashboard },
+];
    
